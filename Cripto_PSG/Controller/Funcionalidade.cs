ac098dcf73e648be52f38602dd5a794085c5f72e
Barretim <bruno.barreto20013@gmail.com>
2022-10-24 11:30:43 -0400

    first commit

---
+++ Cripto_PSG/Controller/Funcionalidade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cripto_PSG.Controller
+{
+    internal class Funcionalidade
+    {
+        internal void Sair()
+        {
+            if (MessageBox.Show("Deseja sair da aplicação?", "SAIR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) return;
+            Application.Exit();
+        }
+        internal void Criptar(TextBox txt_chave, TextBox txt_cripto, TextBox txt_decripto, int cripto)
+        {
+            if (txt_chave.Text == string.Empty)
+            {
+                txt_chave.Text = "1";
+            }
+
+            int chave = Convert.ToInt32(txt_chave.Text);
+
+            txt_decripto.Text = String.Empty;
+            int txtCifrado;
+            for (int i = 0; i < txt_cripto.Text.Length; i++)
+            {
+                int txtUsuario = (int)txt_cripto.Text[i];
+
+                txtCifrado = txtUsuario + (chave * cripto);
+                txt_decripto.Text += char.ConvertFromUtf32(txtCifrado);
+               
+                Clipboard.SetText(txt_decripto.Text);
+            }
+        }
+       
+                
+        internal void Limpar(TextBox txt_chave, TextBox txt_cripto, TextBox txt_decripto)
+        {
+            txt_chave.Text = String.Empty;
+            txt_cripto.Text = String.Empty;
+            txt_decripto.Text = String.Empty;
+        }
+        internal void MensagemValidar()
+        {
+            DialogResult r5 = MessageBox.Show("Digite apenas números de 1 a 9!",
+                 "Mensagem de aviso", MessageBoxButtons.OK,
+                 MessageBoxIcon.Question,
+                 MessageBoxDefaultButton.Button1);
+        }
+    }
+}
---
+++ Cripto_PSG/Interface/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cripto_PSG.Controller;
+
+namespace Cripto_PSG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Funcionalidade controller = new Funcionalidade();
+
+        private void btn_criptar_Click(object sender, EventArgs e)
+        {
+            int cripto = +1;
+
+            controller.Criptar(txt_chave, txt_cripto, txt_decripto, cripto);
+        }
+        private void btn_decriptar_Click(object sender, EventArgs e)
+        {
+            int cripto = -1;
+
+            controller.Criptar(txt_chave, txt_cripto, txt_decripto, cripto);
+        }
+        private void txt_chave_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '0')
+            {
+                e.Handled = true;
+                DialogResult dn = MessageBox.Show("Digite apenas número de 1 a 9", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                DialogResult dn = MessageBox.Show("Somente números", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            controller.Limpar(txt_chave, txt_cripto, txt_decripto);
+        }
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            controller.Sair();
+        }
+        public void message_validator()
+        {
+            controller.MensagemValidar();
+        }
+        private void btn_criptar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                btn_criptar_Click(sender, e);
+            }
+        }
+        private void txt_chave_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_chave.Text.Length > 1)
+            {
+                txt_chave.Text = String.Empty;
+                message_validator();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
